765643e604a885357a3ca8d8839347259c28942d
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2023-11-16 11:06:19 +0100

    chore: initial commit

---
+++ src/components/card-props.ts
@@ -0,0 +1,8 @@
+export interface CardProps {
+  slug: string;
+  title: string;
+  summary: string;
+  image: string;
+  address: string;
+  phone: number;
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as Card } from "./components/card.astro";
+export type { CardProps } from "./components/card-props";
+export * from "./components/card.astro";
